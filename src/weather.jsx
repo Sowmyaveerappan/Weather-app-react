4a9522e74d32893ad9dc77ece3aa33c569db1ac1
Sowmya Bala <sowmyabalamurugan1@gmail.com>
2024-10-04 13:47:16 +0800

    updated css

--- src/weather.jsx
+++ src/weather.jsx
@@ -27,12 +27,12 @@ function Weather()
 
     return(
           <div className="bg-yellow-200 p-20 ">
-            <div className="bg-purple-400 p-20 rounded-md  ">
+            <div className="bg-purple-400 p-10 rounded-md  ">
                 <h1 className="text-3xl font-medium  text-yellow-950">Weather Report:</h1>
                 <input type="text"onChange={handleCity} className="mt-2 border border-black rounded-md p-1"></input><br></br>
                 <button onClick={getWeather}className="bg-black text-white p-2 rounded-md mt-2 hover:bg-yellow-700">Get the Report</button>
 
-                <div className="mt-8">
+                <div className="mt-2">
                     <h1 className="text-2xl mt-5"><b>Weather: </b><span className="text-opacity-100  text-white bg-black font-medium">{weather}</span></h1>
                     <p className="text-2xl mt-5"><b>Temperature: </b><span className="text-opacity-100 text-white bg-black font-medium">{temp}</span></p>
                     <p className="text-2xl mt-5"><b>Description: </b><span className="text-opacity-100 text-white bg-black font-medium">{desc}</span></p>
